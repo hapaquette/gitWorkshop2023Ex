6bd6cf42111d744f35adfc2adc975c3203733d91
Hayley Paquette <hapaquette@mun.ca>
2023-02-27 16:13:38 -0330

    edited script1 with notes on exercise steps.

---
+++ script_1.R
@@ -0,0 +1,12 @@
+## GitHub Workshop Feb 27 2023
+# Exercise 1: created a new directory via file>new project>new directory>new
+# project. Name for directory is 'my_new_repo' (it is a .Rproj) and checked
+# box for 'create a git repository'
+
+# then created a new file, 'script_1' which is a .R file.
+# remember to 'save' often, saving this script resulted in it showing up in the
+# git window with a green A icon and was available to 'commit'
+
+# committed script and gitignore.
+
+# edited script_1
